9cc95a672bf8cfc841209ea62dd4deb6c016a228
Roronoa <Roranoazoro54@gmail.com>
2026-03-16 19:58:58 +0000

    Remove finished tasks

--- NumberGuessingGame/NumberGuessingGame.cpp
+++ NumberGuessingGame/NumberGuessingGame.cpp
@@ -6,6 +6,7 @@
 #include <string>
 
 void GuessNumber(int &randomNumber, int guessNumber);
+void GameDifficulty();
 
 void clearScreen()
 {
@@ -14,27 +15,16 @@ void clearScreen()
 
 int main()
 {
-	// Generate a random number between 1 and 100 (Done)
-	// Use srand(time(0)) to seed the random number generator so its different every turn (Done)
 
 	// PLAYER INPUT
 	// Ask the player to guess the number (Done)
 	// Read it with std::cin (Done)
 
-	// HINT SYSTEM	
-	// If guess is too low -> print "Too low! Try again." (Done)
-	// If guess is too high -> print "Too high! Try again." (Done)
-	// If guess is correct -> print "Congratulations! You guessed the number in X attempts!" and end the game.
-
 	// ATTEMPT COUNTER
 	// Track how many guesses the player has made.
 	// Display it when they win.
 
-	// Use a while loop that keeps running until the player guesses correctly (Done)
-	// One loop = one guess (Done)
-
 	// EXTRAS
-	// Include play again option after winning (Done)
 	// Add difficulty levels -> Easy (10 guesses), Medium (7 guesses), Hard (5 gueses).
 	// Input validation -> Ensure the player enters a number between 1 and 100 or types a letter.
 
@@ -60,14 +50,10 @@ int main()
 
 	// OPTIONAL
 	// Use constants instead of magic numbers. So instead of writing 100, write const int MAX_NUMBER = 100; and use MAX_NUMBER in the code.
-	// Use meaningful variable names. Instead of writing int x; write int playerGuess; or int randomNumber;
-	// Try not to repeat your code. And try to keep main clean.
 
 	// Add a Leaderboard if you can. For now only stores the last 3 highest scores. (so lowest attempt at top)
 
 	// BAD HABITS TO AVOID
-	// Using global variables. Try to keep all your variables inside main or inside functions.
-	// Using goto statements. They can make your code messy and hard to read.
 	// Don't ignore std::cin. Always validate user input.
 
 	return 0;
