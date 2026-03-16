b823ab1d2393b5f7e96e3cb32b7da7e80a214529
Roronoa <Roranoazoro54@gmail.com>
2026-03-16 23:33:47 +0000

    save before merge

--- NumberGuessingGame/NumberGuessingGame.cpp
+++ NumberGuessingGame/NumberGuessingGame.cpp
@@ -6,6 +6,7 @@
 #include <string>
 
 void GuessNumber(int &randomNumber, int guessNumber);
+void GameDifficulty();
 
 void clearScreen()
 {
@@ -14,27 +15,12 @@ void clearScreen()
 
 int main()
 {
-	// Generate a random number between 1 and 100 (Done)
-	// Use srand(time(0)) to seed the random number generator so its different every turn (Done)
-
-	// PLAYER INPUT
-	// Ask the player to guess the number (Done)
-	// Read it with std::cin (Done)
-
-	// HINT SYSTEM	
-	// If guess is too low -> print "Too low! Try again." (Done)
-	// If guess is too high -> print "Too high! Try again." (Done)
-	// If guess is correct -> print "Congratulations! You guessed the number in X attempts!" and end the game.
 
 	// ATTEMPT COUNTER
 	// Track how many guesses the player has made.
 	// Display it when they win.
 
-	// Use a while loop that keeps running until the player guesses correctly (Done)
-	// One loop = one guess (Done)
-
 	// EXTRAS
-	// Include play again option after winning (Done)
 	// Add difficulty levels -> Easy (10 guesses), Medium (7 guesses), Hard (5 gueses).
 	// Input validation -> Ensure the player enters a number between 1 and 100 or types a letter.
 
@@ -52,22 +38,25 @@ int main()
 	{
 		srand(time(0)); // Seed the random number generator
 		int randomNumber = rand() % 100 + 1; // Generate a random number between 1 and 100. +1 is added to shift the range from 0-99 to 1-100
+		//
 		GuessNumber(randomNumber, 0);
-		std::cout << "\nDo you want to play again, if you enter 'no' the game will end? (y/n): ";
-		std::cin >> playAgain;
+		do
+		{
+			std::cout << "\nDo you want to play again, if you enter 'no' the game will end? (y/n): ";
+			std::cin >> playAgain;
+			//std::cin.clear(); Only needed if we are reading a number and the user enters a letter. In this case we are reading a char, so we don't need to clear the input buffer.
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Telling the buffer to ignore everything it possibly can until it finds a newline character "\n". Basically the enter key.
+
+		} while (playAgain != 'y' && playAgain != 'Y' && playAgain != 'n' && playAgain != 'N');
 	}
 	return 0;
 
 	// OPTIONAL
 	// Use constants instead of magic numbers. So instead of writing 100, write const int MAX_NUMBER = 100; and use MAX_NUMBER in the code.
-	// Use meaningful variable names. Instead of writing int x; write int playerGuess; or int randomNumber;
-	// Try not to repeat your code. And try to keep main clean.
 
 	// Add a Leaderboard if you can. For now only stores the last 3 highest scores. (so lowest attempt at top)
 
 	// BAD HABITS TO AVOID
-	// Using global variables. Try to keep all your variables inside main or inside functions.
-	// Using goto statements. They can make your code messy and hard to read.
 	// Don't ignore std::cin. Always validate user input.
 
 	return 0;
@@ -77,10 +66,22 @@ void GuessNumber(int &randomNumber, int guessNumber)
 {
 	clearScreen();
 	std::cout << "Your random number has been generated. Please enter a guess: ";
-	std::cin >> guessNumber;
-
+	
+		
 	while (guessNumber != randomNumber)
 	{
+		std::cin >> guessNumber;
+
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Invalid input, please enter a number: ";
+			continue;
+		}
+
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
 		if (guessNumber < randomNumber)
 		{
 			std::cout << "Too low! Try again: ";
@@ -89,8 +90,10 @@ void GuessNumber(int &randomNumber, int guessNumber)
 		{
 			std::cout << "Too high! Try again: ";
 		}
-		std::cin >> guessNumber;
 	}
+
 	std::cout << "Congratulations! You guessed the number!" << std::endl;
 	std::cout << "The random number was: " << randomNumber << std::endl;
 }
+
+
